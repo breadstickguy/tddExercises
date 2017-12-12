e296d85f73c539897913b76a56a360f7e32324a0
JJ Brenner <brenner.jj@gmail.com>
2017-12-11 20:30:43 -0700

    stuff

--- TDDExercise.js
+++ TDDExercise.js
@@ -3,17 +3,14 @@ const expect = require('chai').expect;
 var x = 42;
 var y = 'bacon';
 var z = [1,2,3,4];
-var h = {name: "JJ", type: "Person"};
+var h = {name:'JJ' , type:'Person'};
 
-var sum = (a,b) => a+b;
-var subtract = (a,b) => a-b;
-var multiply = (a,b) => a*b;
-var divide = (a,b) => a/b;
-var logicCheck = (param) => {
-	if (param) {
-		return true;
-	}
-	return false;
+function sum(bacon,cheese) {
+	return bacon+cheese;
+}
+
+function subtract(a,b){
+	return a-b;
 }
 
 describe("Unit Testing Framework", function() {
@@ -42,15 +39,17 @@ describe("Unit Testing Framework", function() {
 	//Write functions to do math
 	it('shows that we can write functions to do arithmetic', function() {
 		expect(sum(4,2)).to.equal(6);
+		expect(sum(6,4)).to.equal(10);
+		expect(sum(8,15)).to.equal(23);
 		expect(subtract(10,5)).to.equal(5);
-		expect(multiply(3,8)).to.equal(24);
-		expect(divide(9,3)).to.equal(3);
+	// 	expect(multiply(3,8)).to.equal(24);
+	// 	expect(divide(9,3)).to.equal(3);
 	});
 
 	//Write a function to check basic logic
-	it('shows that we can use conditional logic properly', function() {
-		expect(logicCheck(5 == 5)).to.be.true;
-		expect(logicCheck(3 > 7)).to.be.false;
-	});
+	// it('shows that we can use conditional logic properly', function() {
+	// 	expect(logicCheck(5 == 5)).to.be.true;
+	// 	expect(logicCheck(3 > 7)).to.be.false;
+	// });
 
 } );
