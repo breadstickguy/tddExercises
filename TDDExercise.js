edc8d0357ee998c401a38c1c6890f9657b9b40cf
JJ Brenner <jbrenner@solutionstream.com>
2017-12-13 23:46:01 -0700

    adding looping and reverse/addarray

--- TDDExercise.js
+++ TDDExercise.js
@@ -1,21 +1,5 @@
 const expect = require('chai').expect;
 
-var x = 42;
-var y = 'bacon';
-var z = [1,2,3,4];
-var h = {name: "JJ", type: "Person"};
-
-var sum = (a,b) => a+b;
-var subtract = (a,b) => a-b;
-var multiply = (a,b) => a*b;
-var divide = (a,b) => a/b;
-var logicCheck = (param) => {
-	if (param) {
-		return true;
-	}
-	return false;
-}
-
 describe("Unit Testing Framework", function() {
 	it("should properly run tests!", function () {
 		expect(true).to.be.true;
@@ -29,7 +13,7 @@ describe("Unit Testing Framework", function() {
 		expect(h).to.be.an('object');
 	});
 
-	//Assign values to variables to make the tests pass
+	// //Assign values to variables to make the tests pass
 	it('shows we can assign values to variables', function () {
 		expect(x).to.equal(42);
 		expect(y).to.equal('bacon');
@@ -39,30 +23,40 @@ describe("Unit Testing Framework", function() {
 		
 	});
 
-	//Write functions to do math
+	// //Write functions to do math
 	it('shows that we can write functions to do arithmetic', function() {
+	 	//Write a function that adds numbers together
 		expect(sum(4,2)).to.equal(6);
-		expect(subtract(10,5)).to.equal(5);
-		expect(multiply(3,8)).to.equal(24);
-		expect(divide(9,3)).to.equal(3);
+		expect(sum(4,5)).to.equal(9);
+	// 	//Write a function that subtracts numbers
+	// 	expect(subtract(10,5)).to.equal(5);
+	//	expect(subtract(15,6)).to.equal(9);
+	//	Write a function that multiplies two numbers
+	// 	expect(multiply(3,8)).to.equal(24);
+	// 	expect(divide(9,3)).to.equal(3);
 	});
 
-	//Write a function to check basic logic
-	it('shows that we can use conditional logic properly', function() {
-		expect(logicCheck(5 == 5)).to.be.true;
-		expect(logicCheck(3 > 7)).to.be.false;
-	});
+	// //Write a function to check basic logic
+	// it('shows that we can use conditional logic properly', function() {
+	// 	expect(logicCheck(5 == 5)).to.be.true;
+	// 	expect(logicCheck(3 > 7)).to.be.false;
+	// });
 
-		var input = [1, 2, 3, 4, 5, 6];
-		var output = [10 ,20, 30, 40, 50, 60];
-		function testForLoop(input, output){
-			it('shows we can use a for loop to repeat tasks', function() {
-				expect(input * 10).to.equal(output);
-			});
-		}
-		for(var i = 0; i < input.length; i++){
-			testForLoop(input[i], output[i]);
-		}
+	// 	var input = [1, 2, 3, 4, 5, 6];
+	// 	var output = [10 ,20, 30, 40, 50, 60];
+	// 	function testForLoop(input, output){
+	// 		it('shows we can use a for loop to repeat tasks', function() {
+	// 			expect(input * 10).to.equal(output);
+	// 		});
+	// 	}
+
+
+	//  it('shows we can write and call functions', function() {
+	//  	//Write a function that reverses a string
+	//  	expect(reverseAString('hello')).to.equal('olleh');
+	//  	//Write a function that sums the values in an array
+	//  	expect(addArray([1,2,3,4,5])).to.equal(15);
+	//  })
 
 });
 
